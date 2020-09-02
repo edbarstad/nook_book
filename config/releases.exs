092d47b08f04d715b3b95d123d4b71a0a4a234c3
Ed Barstad <ed@barstad.com>
2020-09-02 14:35:23 -0600

    Step 21

--- config/releases.exs
+++ config/releases.exs
@@ -1,7 +1,24 @@
 import Config
 
+name = "ejb"
+
+config :libcluster,
+  topologies: [
+    nook_book: [
+      strategy: Cluster.Strategy.Empd,
+      config: [
+        hosts: [
+          :"nook_book@10.0.1.5",
+          :"nook_book@10.0.1.125"
+        ]
+      ]
+    ]
+  ]
+
+config :nook_book, cluster_role: System.get_env("CLUSTER_ROLE", "member") |> String.to_atom()
+
 config :nook_book, NookBookWeb.Endpoint,
   server: true,
   http: [port: 4000],
-  url: [host: "localhost"],
+  url: [host: "#{name}.nookbook.online"],
   secret_key_base: "pbHD1YpDeMBnt28fSGN1hwKZkUe7kRrlFoLZwdG3gBOUO0Vso1U3kzclW1qJFG+t"
